1533b586b0c6c6c70e054e98e89bd21610b5f08e
hung <hungnbc2@gmai.com>
2023-07-03 14:42:11 +0700

    finish 2

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,12 +1,22 @@
 const Note = require('../models/List');
 
 class MeController {
+
+    
     show(req,res,next){
-        Note.find({}).lean()
-            .then((notes)=>{
-                notes = notes.sort((a,b)=>a.deadline-b.deadline);
-                res.render('me/show',{ notes })})
-            .catch(next)
+        let NoteQuery= Note.find({}).lean();
+        if(req.query.hasOwnProperty('_sort'))
+        {
+            NoteQuery = NoteQuery.sort({
+                [req.query.column] : req.query.type
+            })
+        }
+
+       Promise.all([NoteQuery])
+        .then(([notes])=>{
+            res.render('me/show',{ notes })
+         })
+            
     }
     trash(req,res,next){
         Note.findDeleted({}).lean()
--- src/server.js
+++ src/server.js
@@ -2,15 +2,17 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars').engine;
 const db = require('./config/db');
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
+const SortMiddleware = require('./app/Middlewares/SortMiddleware');
 
 db.connect();
 
 const app = express()
 const port = 3000
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'))
 const route = require('./routes')
+app.use(SortMiddleware)
 
 app.use(
     express.urlencoded({
@@ -25,6 +27,22 @@ app.use(
       extname: '.hbs',
       helpers: {
         sum: (a, b) => a + b,
+        sortable: (key,sort) =>{
+          const sortType = key === sort.column ? sort.type: 'default'
+          const icons = {
+            default : 'oi oi-elevator',
+            asc : 'oi oi-sort-ascending',
+            desc: 'oi oi-sort-descending'
+          }
+          const types = {
+            default :'desc',
+            asc:'desc',
+            desc:'asc'
+          }
+          const icon = icons[sortType];
+          const type = types[sortType];
+          return`<a href="?_sort&column=${key}&type=${type}"><span class="${icon}"></span></a>`;
+        }
       },
     }),
   );
